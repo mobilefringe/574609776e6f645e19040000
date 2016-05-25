f5d169b219e53d57645cd6dcbddc39db59b7eda0
ardalan@mobilefringe.com @ CodeCloud <ardalan@mobilefringe.com @ CodeCloud>
2016-05-25 17:40:40 -0400

    Pacific Commons 1.0  /site.js

--- site.js
+++ site.js
@@ -2,5 +2,5 @@ var site_json = {
                     "name" : "The District at Green Valley Ranch",
                     "default_image" : "hhttp://assets.codecloudapp.com/sites/574609776e6f645e19040000/image/jpeg/undefined/pc_500x500_logo.jpg",
                     "time_zone" : "T08:00:00Z",
-                    "instagram" : "196558634"
+                    "instagram" : "1429434108"
                 }
