0a0deaa157dc036c3f7661dbef2097c822142896
ardalan@mobilefringe.com @ CodeCloud <ardalan@mobilefringe.com @ CodeCloud>
2016-05-26 11:01:01 -0400

    Pacific Commons 1.0  /site.js

--- site.js
+++ site.js
@@ -1,6 +1,6 @@
 var site_json = {
                     "name" : "Pacific Commons",
-                    "default_image" : "hhttp://assets.codecloudapp.com/sites/574609776e6f645e19040000/image/jpeg/undefined/pc_500x500_logo.jpg",
+                    "default_image" : "http://assets.codecloudapp.com/sites/574609776e6f645e19040000/image/jpeg/undefined/pc_500x500_logo.jpg",
                     "time_zone" : "T08:00:00Z",
                     "instagram" : "1429434108"
                 }
