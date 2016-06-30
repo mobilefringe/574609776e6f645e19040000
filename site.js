4aa5b29757537ee5b01251a404c9457587442283
ardalan@mobilefringe.com @ CodeCloud <ardalan@mobilefringe.com @ CodeCloud>
2016-06-30 11:47:45 -0400

    Pacific Commons 1.0  /site.js

--- site.js
+++ site.js
@@ -2,5 +2,5 @@ var site_json = {
                     "name" : "Pacific Commons",
                     "default_image" : "http://assets.codecloudapp.com/sites/574609776e6f645e19040000/image/jpeg/undefined/pc_500x500_logo.jpg",
                     "time_zone" : "T08:00:00Z",
-                    "instagram" : "1429434108"
+                    "social_feed" : "http://pacificcommons.mallmaverick.com/api/v2/pacificcommons/social.json"
                 }
