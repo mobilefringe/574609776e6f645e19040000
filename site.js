4142cff1349fa3d6185a3b6fbe00273474cd568b
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2016-10-04 15:58:40 -0400

    Pacific Commons 1.1  /site.js

---
+++ site.js
@@ -0,0 +1,6 @@
+var site_json = {
+                    "name" : "Pacific Commons",
+                    "default_image" : "//codecloud.cdn.speedyrails.net/sites/57f405a16e6f64554f6a0000/image/jpeg/undefined/pc_500x500_logo.jpg",
+                    "time_zone" : "T08:00:00Z",
+                    "social_feed" : "//pacificcommons.mallmaverick.com/api/v2/pacificcommons/social.json"
+                }
